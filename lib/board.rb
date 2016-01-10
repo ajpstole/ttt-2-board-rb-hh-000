ef798b8617f80298d4a9d12f65525a54420246fb
ajpstole <anthonyjpstole@gmail.com>
2016-01-10 06:18:45 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -9,6 +9,14 @@ row[1]
 # Position 3: Right
 row[2]
 
+# Move X to the Position 1, Middle
+row[0] = "X"
+row #=> ["X", " ", " "]
+
 # Move X to the Position 2, Middle
 row[1] = "X"
 row #=> [" ", "X", " "]
+
+# Move X to the Position 3, Middle
+row[2] = "X"
+row #=> [" ", " ", "X"]
